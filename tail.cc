55e74e1d4b16fe299e231c2034d1200745075f56
hansfilipelo <hansfilipelo@gmail.com>
2014-09-12 08:16:16 +0200

    Trouble finding numbers. Needs work.

--- tail.cc
+++ tail.cc
@@ -1,23 +1,45 @@
 
 #include <list>
 #include <vector>
+#include <string>
 #include <iostream>
 using namespace std;
+
+// -------- Helper funcitons
+
+
+bool isNumber(const string& inString){
+    cout << inString << endl;
+    for (int i = 0; i < inString.size(); i++) {
+        if ( (int)inString.at(i) > 9 ) {
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+// -------------------------
 
 int main(int argc, char *argv[])
 {
     int nrOfLines = 25;
-    vector<char*> filePaths;
+    vector<string> filePaths;
     
     // Read args to vector
-    vector<char*> argVector;
+    vector<string> argVector;
     for (int i = 0; i < argc; i++) {
-        argVector.insert(argVector.begin(),static_cast<char*>(argv[i]));
+        argVector.insert(argVector.begin(),static_cast<string>(argv[i]));
+    }
+    
+    cout << "Args: " << endl;
+    for (int i = 0; i < argVector.size(); i++) {
+        cout << argVector.at(i) << endl;
     }
     
     // Check if user needs help
     for (int i = 0; i < argVector.size(); i++) {
-        if ( !strncmp(argVector.at(i),"-h",2) || !strncmp(argVector.at(i),"--help",6) ) {
+        if ( argVector.at(i) == "-h" || argVector.at(i) == "--help" ) {
             argVector.erase(argVector.begin()+i);
             cout << "User needs help" << endl;
         }
@@ -25,21 +47,24 @@ int main(int argc, char *argv[])
     
     // Check for if nr of lines are given.
     for (int i = 0; i < argVector.size(); i++) {
-        if (!strncmp(argVector.at(i),"-n",2)) {
-            // Remove -n from args
-            argVector.erase(argVector.begin()+i);
-            // Get nrOfLines
-            nrOfLines = atoi(argVector.at(i));
+        if ( argVector.at(i) == "-n" ) {
+            // Get nrOfLines - need to find an exception if char isn't a number
+            if ( isNumber(argVector.at(i-1)) ) {
+                nrOfLines = atoi(argVector.at(i-1).c_str());
+            }
+            else {
+                cout << "Parameter after '-n' needs to be a valid number." << endl;
+            }
             // Remove nrOfLines from args
             argVector.erase(argVector.begin()+i);
+//            argVector.erase(argVector.begin()+i);
         }
     }
     
     // Rest of args are filepaths.
     filePaths = argVector;
-    delete argVector;
     
-    for (int i = 0; i < filePaths.size(); i++) {
-        <#statements#>
-    }
+//    for (int i = 0; i < filePaths.size(); i++) {
+//        <#statements#>
+//    }
 }
